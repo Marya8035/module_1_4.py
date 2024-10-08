f0948a424c01d9c9a56160f470c764d3d9bd0659
Marya8035 <macha.zyrina@gmail.com>
2024-10-08 15:01:04 +0500

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+my_string = input("Название домашнего задания: ") #вводим Организация программ и методы строк
+print(type(my_string)) #парисваеваем значени строки
+print(my_string[0:35]) #выводим количество символов текста
+print(my_string.upper()) #выводим строку в верхний регистр
+print(my_string.lower()) #выводим строку в нижний регистр
+print(my_string.replace( " ", ""))
+print(my_string[0])
+print(my_string[-1])
